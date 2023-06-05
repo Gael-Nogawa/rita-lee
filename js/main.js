3d10148aa5fa6c06d9f012ed0bc48c45a048ee0e
Gael Eiji Nogawa <gael.nogawa@gmail.com>
2023-06-05 11:09:15 -0300

    adicionando melhoras na reponsabilidade e acessibilidade

---
+++ js/main.js
@@ -0,0 +1,49 @@
+
+$(document).ready(function(){
+
+    $('#telefone').mask('(00) 00000-0000',{ 
+        placeholder: '(12) 98765-4321'    
+    })
+
+    $('form').validate({
+        rules: {
+            nome: {
+                required: true,
+                rangelength: [2,50]
+            },
+            email: {
+                required: true,
+                email: true
+            },
+            telefone: {
+                required: false
+            },
+            mensagem: {
+                required: true
+            }
+        },
+        messages: {
+            nome: 'Por favor, insira no nome',
+            email: 'Por favor, insira o seu melhor e-mail',
+            mensagem: 'Por favor, insira a sua mensagem'
+        },
+
+        
+        submitHandler: function(form){
+            alert('Enviado com sucesso!');
+            
+        
+        },
+        invalidHandler: function(evento, validador){
+            let camposInvalidos = validador.numberOfInvalids()
+            if(camposInvalidos){
+                alert(`Existem ${camposInvalidos} campos inválidos`);
+            }
+        }
+        
+    })
+
+    $('form').val('');
+})
+
+
